b2ad35b6be06bcc5f99eac6e3e94413c69a0dfa6
rufuspenri-cell <rufuspenri@gmail.com>
2026-08-06 13:53:24 +0100

    Improved visualisation

--- fluid.py
+++ fluid.py
@@ -8,16 +8,26 @@ density = np.zeros((HEIGHT, WIDTH))
 vx = np.zeros((HEIGHT, WIDTH))
 vy = np.zeros((HEIGHT, WIDTH))
 
+def sample_density(density, x, y):
+	x = max(0, min(x, WIDTH - 1))
+	y = max(0, min(y, HEIGHT - 1))
+	x0 = int(np.floor(x))
+	y0 = int(np.floor(y))
+	x1 = min(x0 + 1, WIDTH - 1)
+	y1 = min(y0 + 1, HEIGHT - 1)
+	sx = x - x0
+	sy = y - y0
+	top = (1 - sx) * density[y0, x0] + sx * density[y0, x1]
+	bottom = (1 - sx) * density[y1, x0] + sx * density[y1, x1]
+	return (1 - sy) * top + sy * bottom
+
 def advect(density, vx, vy, dt):
 	new_density = np.zeros_like(density)
 	for y in range(HEIGHT):
 		for x in range(WIDTH):
 			old_x = x - vx[y, x] *dt
 			old_y = y - vy[y, x] *dt
-			old_x = int(round(old_x))
-			old_y = int(round(old_y))
-			if 0 <= old_x < WIDTH and 0 <= old_y < HEIGHT:
-				new_density[y, x] = density[old_y, old_x]
+			new_density[y, x] = sample_density(density, old_x, old_y)
 	return new_density
 
 cx = WIDTH / 2
